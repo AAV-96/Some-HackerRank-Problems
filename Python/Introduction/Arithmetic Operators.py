aa9b9b017568db9fb83477f5de30b970ff8f7e8f
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2016-08-21 23:09:03 +0530

    Create Arithmetic Operators.py

---
+++ Python/Introduction/Arithmetic Operators.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(raw_input())
+b=int(raw_input())
+print(a+b)
+if (a>b):
+    print(a-b)
+else:
+    print(b-a)
+print(a*b)    
