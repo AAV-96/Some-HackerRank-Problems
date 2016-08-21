fcc660b11491a95c2ae9d6c182735a038ced36bc
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2016-08-21 22:21:32 +0530

    Create Java Static Initializer Block.java

---
+++ Java/Java Static Initializer Block.java
@@ -0,0 +1,14 @@
+/*only this much code is to be filled*/
+
+static boolean flag = true;
+static int B, H;
+
+static {    
+    Scanner sc = new Scanner(System.in);
+    B = sc.nextInt();
+    H = sc.nextInt();
+    if (B <= 0 || H <= 0) {
+        flag = false;
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+}
