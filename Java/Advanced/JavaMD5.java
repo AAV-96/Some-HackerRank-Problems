2628e76b174123b929ab037d26c2aa6becd8e675
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2017-06-15 15:35:50 +0530

    Create JavaMD5.java

---
+++ Java/Advanced/JavaMD5.java
@@ -0,0 +1,22 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.*;
+import java.util.*;
+
+public class build_demo {
+
+	public static void main(String[] args) throws IOException, Exception {
+		Scanner sc = new Scanner(System.in);
+        String org = sc.next();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(org.getBytes());
+        byte[] digest = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        System.out.println(sb.toString());
+        sc.close();
+	}
+
+}
