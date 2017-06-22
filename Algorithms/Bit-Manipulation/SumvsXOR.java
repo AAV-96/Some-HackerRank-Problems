5967a10b7010a40f6ca158aa91d4677ba1d4e2df
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2017-06-22 18:21:52 +0530

    Create SumvsXOR.java

---
+++ Algorithms/Bit-Manipulation/SumvsXOR.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long solve(long n) {
+        long ctr = 0L;
+        /*for (long i = 0L; i<=n; i++) {
+            if ((n+i) == (n^i)) {
+                ctr++;
+            }
+        }*/
+        while (n > 0) {
+            ctr += (n%2 == 0)?1:0;
+            n /= 2;
+        }
+        return (long)Math.pow(2, ctr);       
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long result = solve(n);
+        System.out.println(result);
+    }
+}
